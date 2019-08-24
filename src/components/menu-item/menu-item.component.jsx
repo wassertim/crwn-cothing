302346e99b1b5bbac23edfbe90f094f9c9445457
Tim Tkachenko <tim.tkachenko@idealo.de>
2019-08-24 10:49:55 +0200

    add HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import {withRouter} from 'react-router-dom';
+
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
-        <div style={{
-            backgroundImage: `url(${imageUrl})`
-        }} className="background-image"></div>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+        <div
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+            className="background-image"
+        />
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
@@ -15,4 +20,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
 );
 
 
-export default MenuItem;
+export default withRouter(MenuItem);
