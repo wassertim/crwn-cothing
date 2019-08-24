99d82e2520d82539d7038648f2c103f549403046
Tim Tkachenko <tim.tkachenko@idealo.de>
2019-08-24 11:21:36 +0200

    style collection preview

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const TopicsList = (props) => {
-    console.log(props)
-    return (<div>
-        <h1>TOPICS LIST</h1>
-    </div>);
-};
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
     return <div>
-        <Route exact path="/" component={HomePage}/>
-        <Route path="/shop/hats" component={TopicsList}/>
-        {/*<Route path="/topics/:topicId" component={TopicDetail}/>*/}
+        <Switch>
+            <Route exact path="/" component={HomePage}/>
+            <Route path="/shop" component={ShopPage}/>
+        </Switch>
     </div>;
 }
 
