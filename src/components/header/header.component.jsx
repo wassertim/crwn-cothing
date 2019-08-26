ea7b7e84c45cebdc36bdf217a8e4d83af088ac43
Tim Tkachenko <tim.tkachenko@idealo.de>
2019-08-26 21:46:07 +0200

    style auth buttons

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import {auth} from '../../firebase/firebase.utils';
+
 import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 
-const Header = () => (
+const Header = ({currentUser}) => (
     <div className="header">
         <Link to="/">
             <Logo className="logo"/>
@@ -12,6 +14,12 @@ const Header = () => (
         <div className="options">
             <Link to="/shop" className="option">SHOP</Link>
             <Link to="/contact" className="option">CONTACT</Link>
+            {
+                currentUser ?
+                    <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                    :
+                    <Link className='option' to="/signin">SIGN IN</Link>
+            }
         </div>
     </div>
 );
