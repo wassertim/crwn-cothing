36db553af717c497e93644ec44707673be1d8def
Tim Tkachenko <tim.tkachenko@idealo.de>
2019-08-28 21:40:53 +0200

    add cart icon component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import {auth} from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
 
 import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
@@ -21,6 +22,7 @@ const Header = ({currentUser}) => (
                     :
                     <Link className='option' to="/signin">SIGN IN</Link>
             }
+            <CartIcon />
         </div>
     </div>
 );
