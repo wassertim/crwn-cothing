b39bc3f97ce516ab20be2255fc9f8b3075f59828
Tim Tkachenko <tim.tkachenko@idealo.de>
2019-08-31 23:27:11 +0200

    close the dropdown automatically

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -8,8 +8,9 @@ import CartItem from '../cart-item/cart-item.component';
 import {selectCartItems} from '../../redux/cart/cart.selectors';
 import {createStructuredSelector} from 'reselect';
 import {withRouter} from 'react-router-dom';
+import {toggleCartHidden} from '../../redux/cart/cart.actions';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
       <div className="cart-items">
           {
@@ -18,7 +19,10 @@ const CartDropdown = ({ cartItems, history }) => (
                   : (<span className="empty-message">Your cart is empty</span>)
           }
       </div>
-      <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+      <CustomButton onClick={() => {
+          history.push('/checkout');
+          dispatch(toggleCartHidden());
+      }}>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
