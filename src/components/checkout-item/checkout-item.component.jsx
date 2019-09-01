49a29300b1530beb4d08f8cc18fdf27709baeb3d
Tim Tkachenko <tim.tkachenko@idealo.de>
2019-09-01 22:40:42 +0200

    migrate several components

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 
-import './checkout-item.styles.scss';
 import {addItem, clearItemFromCart, removeItem} from '../../redux/cart/cart.actions';
 import {connect} from 'react-redux';
+import {
+    CheckoutItemContainer,
+    ImageContainer,
+    QuantityContainer,
+    RemoveButton,
+    TextContainer
+} from './checkout-item.styles';
 
 const CheckoutItem = ({cartItem, clearItem, addItem, removeItem}) => {
     const {name, imageUrl, price, quantity} = cartItem;
     return (
-        <div className="checkout-item">
-            <div className="image-container">
+        <CheckoutItemContainer>
+            <ImageContainer>
                 <img src={imageUrl} alt="item"/>
-            </div>
-            <span className="name">{name}</span>
-            <span className="quantity">
+            </ImageContainer>
+            <TextContainer>{name}</TextContainer>
+            <QuantityContainer>
                 <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
                     <span className="value">{quantity}</span>
                 <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
-            </span>
-            <span className="price">{price}</span>
-            <span className="remove-button" onClick={() => clearItem(cartItem)}>&#10005;</span>
-        </div>
+            </QuantityContainer>
+            <TextContainer>{price}</TextContainer>
+            <RemoveButton onClick={() => clearItem(cartItem)}>&#10005;</RemoveButton>
+        </CheckoutItemContainer>
     );
 };
 
